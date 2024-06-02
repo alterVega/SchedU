c6faab19959e9ac5744209c69aa68e6412f10d5e
alterVega <164298842+alterVega@users.noreply.github.com>
2024-06-02 11:24:54 +0800

    Update calendar.jsx

--- app/(tabs)/schedule_components/calendar.jsx
+++ app/(tabs)/schedule_components/calendar.jsx
@@ -2,66 +2,36 @@ import React, { useState, Fragment, useCallback, useMemo, useRef } from "react";
 import { StyleSheet, View, Text, TouchableOpacity } from "react-native";
 import { Calendar, CalendarUtils } from "react-native-calendars";
 import { StatsPortion } from "./stats";
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
-export function CalendarPortion() {
-  const INITIAL_DATE = new Date();
+export const CalendarPortion = ({navigation}) => {
+  const [selected, setSelected] = useState('');
+  const currentDate = '2024-01-01';
 
   const getDate = (count) => {
-    const date = new Date(INITIAL_DATE);
+    const date = new Date(currentDate);
     const newDate = date.setDate(date.getDate() + count);
     return CalendarUtils.getCalendarDateString(newDate);
-  };
+  }
 
   return (
-    <View showsVerticalScrollIndicator={false} style={styles.container}>
-      <Fragment>
-        <Text style={styles.text}>Calendar with multi-period marking</Text>
-        <Calendar
-          style={styles.calendar}
-          current={INITIAL_DATE.toString()}
-          markingType={"multi-period"}
-          markedDates={{
-            [INITIAL_DATE]: {
-              periods: [
-                { startingDay: true, endingDay: false, color: "green" },
-                { startingDay: true, endingDay: false, color: "orange" },
-              ],
-            },
-            [getDate(1)]: {
-              periods: [
-                { startingDay: false, endingDay: true, color: "green" },
-                { startingDay: false, endingDay: true, color: "orange" },
-                { startingDay: true, endingDay: false, color: "pink" },
-              ],
-            },
-            [getDate(3)]: {
-              periods: [
-                { startingDay: true, endingDay: true, color: "orange" },
-                { color: "transparent" },
-                { startingDay: false, endingDay: false, color: "pink" },
-              ],
-            },
-          }}
-        />
-      </Fragment>
-      <View style={styles.statsContainer}>
-        <StatsPortion />
-      </View>
-    </View>
+    <Calendar
+      style={{
+        borderWidth: 1,
+        borderColor: 'red',
+        height: 700
+      }}
+      // Selection of date by the user
+      onDayPress={day => {
+        console.log('selected day', day);
+        setSelected(day.dateString);
+        navigation.navigate("Agenda")
+      }}
+      // Mark specific dates as marked
+      markedDates={{
+        [selected] : {selected: true, marked: true, selectedColor: 'red'}
+      }}
+    />
   );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flexDirection: "column",
-    flex: 1,
-  },
-  calendar: {
-    marginBottom: 0,
-    marginTop: 3,
-    flexDirection: "column",
-  },
-  statsContainer: {
-    flex: 1,
-  },
-});
+};
