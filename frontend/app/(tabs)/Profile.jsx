e96c9d09202e115ebda7e0b6c4694c47e677057d
nivek <94969451+kevintanjc@users.noreply.github.com>
2024-06-21 16:19:50 +0800

    Update Profile.jsx

--- frontend/app/(tabs)/Profile.jsx
+++ frontend/app/(tabs)/Profile.jsx
@@ -1,83 +1,94 @@
-import { StyleSheet, Image, Platform, ImageBackground } from 'react-native';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
-import {View, Text} from "react-native";
-import FeatherIcon from 'react-native-vector-icons/Feather';
-import Ionicon from 'react-native-vector-icons/Ionicons';
-import MaterialIcon from 'react-native-vector-icons/MaterialCommunityIcons';
-import Octicon from 'react-native-vector-icons/Octicons'
+import { StyleSheet, Image, Platform, ImageBackground } from "react-native";
+import { ThemedText } from "@/components/ThemedText";
+import { ThemedView } from "@/components/ThemedView";
+import { View, Text } from "react-native";
+import FeatherIcon from "react-native-vector-icons/Feather";
+import Ionicon from "react-native-vector-icons/Ionicons";
+import MaterialIcon from "react-native-vector-icons/MaterialCommunityIcons";
+import Octicon from "react-native-vector-icons/Octicons";
 
 export default function Profile() {
   return (
-      <ImageBackground source = {require('@/assets/images/noticeboardbg.png')} style={styles.screen}>
-        <View style={styles.profile}>
-          <FeatherIcon name="user" size={100}/>
-        </View>
-        <ImageBackground source = {require('@/assets/images/profileBackground.png')} style={styles.paper}>
-          <View style={styles.workTracker}>
-            <View>
-              <Ionicon name='hourglass' size={60} color='brown'/>
-            </View>
-            <View style={styles.label}> 
-            <ThemedText type='subtitle' fontSize>Weekly hours</ThemedText>
-            </View>
+    <ImageBackground
+      source={require("@/assets/images/noticeboardbg.png")}
+      style={styles.screen}
+    >
+      <View style={styles.profile}>
+        <FeatherIcon name="user" size={100} />
+      </View>
+      <ImageBackground
+        source={require("@/assets/images/profileBackground.png")}
+        style={styles.paper}
+      >
+        <View style={styles.workTracker}>
+          <View>
+            <Ionicon name="hourglass" size={60} color="brown" />
           </View>
-      <View style={styles.projector}>
-        <View>
-          <MaterialIcon name='projector' size={60} color='black' />
+          <View style={styles.label}>
+            <ThemedText type="subtitle" fontSize>
+              Weekly hours
+            </ThemedText>
+          </View>
         </View>
-        <View style={styles.label}> 
-            <ThemedText type='subtitle' fontSize>Workload projector</ThemedText>
+        <View style={styles.projector}>
+          <View>
+            <MaterialIcon name="projector" size={60} color="black" />
+          </View>
+          <View style={styles.label}>
+            <ThemedText type="subtitle" fontSize>
+              Workload projector
+            </ThemedText>
+          </View>
         </View>
-      </View>
-      <View style={styles.customization}>
-        <View>
-          <Octicon name='paintbrush' size={60} color='purple'/>
+        <View style={styles.customization}>
+          <View>
+            <Octicon name="paintbrush" size={60} color="purple" />
+          </View>
+          <View style={styles.label}>
+            <ThemedText type="subtitle" fontSize>
+              Design
+            </ThemedText>
+          </View>
         </View>
-        <View style={styles.label}> 
-            <ThemedText type='subtitle' fontSize>Design</ThemedText>
-        </View>
-      </View>
       </ImageBackground>
-      </ImageBackground>
-
+    </ImageBackground>
   );
 }
 
 const styles = StyleSheet.create({
   profile: {
     marginTop: 50,
-    marginLeft: 40
+    marginLeft: 40,
   },
   label: {
-    alignSelf: 'left',
+    alignSelf: "left",
     fontSize: 50,
-    marginTop: 20
+    marginTop: 20,
   },
   paper: {
     marginTop: 20,
-    flex: 1
+    flex: 1,
   },
   workTracker: {
-    flexDirection: 'row',
+    flexDirection: "row",
     marginTop: 60,
     marginLeft: 60,
   },
   projector: {
     marginTop: 45,
     marginLeft: 60,
-    flexDirection: 'row'
+    flexDirection: "row",
   },
   customization: {
     marginTop: 45,
     marginLeft: 60,
-    flexDirection: 'row'
+    flexDirection: "row",
   },
   screen: {
-    backgroundColor: '#FFFFFF',
+    backgroundColor: "#FFFFFF",
     flex: 1,
   },
   font: {
-    fontWeight: 'bold',
-  }
+    fontWeight: "bold",
+  },
 });
