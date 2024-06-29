687fac28e217f8fc14a99efe4d4554da773f4b48
alterVega <164298842+alterVega@users.noreply.github.com>
2024-06-29 22:35:13 +0800

    Update Profile.jsx

--- frontend/app/(tabs)/Profile.jsx
+++ frontend/app/(tabs)/Profile.jsx
@@ -1,83 +1,120 @@
-import { StyleSheet, Image, Platform, ImageBackground } from 'react-native';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
-import {View, Text} from "react-native";
-import FeatherIcon from 'react-native-vector-icons/Feather';
-import Ionicon from 'react-native-vector-icons/Ionicons';
-import MaterialIcon from 'react-native-vector-icons/MaterialCommunityIcons';
-import Octicon from 'react-native-vector-icons/Octicons'
+import { StyleSheet, Image, Platform, ImageBackground, TouchableOpacity, Modal, Button} from "react-native";
+import { ThemedText } from "@/components/ThemedText";
+import { ThemedView } from "@/components/ThemedView";
+import { View, Text } from "react-native";
+import FeatherIcon from "react-native-vector-icons/Feather";
+import Ionicon from "react-native-vector-icons/Ionicons";
+import MaterialIcon from "react-native-vector-icons/MaterialCommunityIcons";
+import Octicon from "react-native-vector-icons/Octicons";
+import { useState } from 'react';
+
+async function getProjector() {
+  const result = await db.getAllAsync(`SELECT DATEDIFF(startTime, endTime) AS duration FROM EVENTS
+    WHERE `);
+  setTransaction(result);
+}
 
 export default function Profile() {
+  async function getProjector() {
+    const result = await db.getAllAsync(`SElECT * FROM EVENTS`)
+  }
+  const [ModalVisible, setModalVisible] = useState(false);
   return (
-      <ImageBackground source = {require('@/assets/images/noticeboardbg.png')} style={styles.screen}>
-        <View style={styles.profile}>
-          <FeatherIcon name="user" size={100}/>
-        </View>
-        <ImageBackground source = {require('@/assets/images/profileBackground.png')} style={styles.paper}>
-          <View style={styles.workTracker}>
-            <View>
-              <Ionicon name='hourglass' size={60} color='brown'/>
-            </View>
-            <View style={styles.label}> 
-            <ThemedText type='subtitle' fontSize>Weekly hours</ThemedText>
-            </View>
+    <ImageBackground
+      source={require("@/assets/images/noticeboardbg.png")}
+      style={styles.screen}
+    >
+      <View style={styles.profile}>
+        <FeatherIcon name="user" size={100} />
+      </View>
+      <ImageBackground
+        source={require("@/assets/images/profileBackground.png")}
+        style={styles.paper}
+      >
+        <View style={styles.workTracker}>
+          <View>
+            <Ionicon name="hourglass" size={60} color="brown" />
           </View>
-      <View style={styles.projector}>
-        <View>
-          <MaterialIcon name='projector' size={60} color='black' />
+          <View style={styles.label}>
+            <ThemedText type="subtitle" fontSize>
+              Weekly hours
+            </ThemedText>
+          </View>
         </View>
-        <View style={styles.label}> 
-            <ThemedText type='subtitle' fontSize>Workload projector</ThemedText>
+        <View style={styles.projector}>
+          <View>
+            <MaterialIcon name="projector" size={60} color="black" />
+          </View>
+          <TouchableOpacity style={styles.label} onPress= {() => setModalVisible(true)}>
+            <ThemedText type="subtitle" fontSize>
+              Workload projector
+            </ThemedText>
+          </TouchableOpacity>
+          <Modal visible={ModalVisible} animationType='slide' presentationStyle='pageSheet' >
+            <View style ={styles.projectorPop}>
+              <ThemedText type ='subtitle'>
+                Your expected workload for the week 
+              </ThemedText>
+              <Button
+                title='Close'
+                color='midnightblue'
+                onPress={() => setModalVisible(false)}
+                />
+            </View>
+          </Modal>
         </View>
-      </View>
-      <View style={styles.customization}>
-        <View>
-          <Octicon name='paintbrush' size={60} color='purple'/>
+        <View style={styles.customization}>
+          <View>
+            <Octicon name="paintbrush" size={60} color="purple" />
+          </View>
+          <View style={styles.label}>
+            <ThemedText type="subtitle" fontSize>
+              Design
+            </ThemedText>
+          </View>
         </View>
-        <View style={styles.label}> 
-            <ThemedText type='subtitle' fontSize>Design</ThemedText>
-        </View>
-      </View>
       </ImageBackground>
-      </ImageBackground>
-
+    </ImageBackground>
   );
 }
 
 const styles = StyleSheet.create({
   profile: {
     marginTop: 50,
-    marginLeft: 40
+    marginLeft: 40,
   },
   label: {
-    alignSelf: 'left',
+    alignSelf: "left",
     fontSize: 50,
-    marginTop: 20
+    marginTop: 20,
   },
   paper: {
     marginTop: 20,
-    flex: 1
+    flex: 1,
   },
   workTracker: {
-    flexDirection: 'row',
+    flexDirection: "row",
     marginTop: 60,
     marginLeft: 60,
   },
   projector: {
     marginTop: 45,
     marginLeft: 60,
-    flexDirection: 'row'
+    flexDirection: "row",
+  },
+  projectorPop: {
+    backgroundColor: 'white',
   },
   customization: {
     marginTop: 45,
     marginLeft: 60,
-    flexDirection: 'row'
+    flexDirection: "row",
   },
   screen: {
-    backgroundColor: '#FFFFFF',
+    backgroundColor: "#FFFFFF",
     flex: 1,
   },
   font: {
-    fontWeight: 'bold',
-  }
+    fontWeight: "bold",
+  },
 });
